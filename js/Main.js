61436e68b57366488486d69ae11bb657d2167bb6
Logan Hendricks <Logan.Hendricks@sdl.usu.edu>
2020-11-12 09:05:51 -0700

    allowed for setters of individual data

--- js/Main.js
+++ js/Main.js
@@ -2,6 +2,13 @@ class Main {
     constructor() {
         this.dataType = "movies";
         this.chartType = "bar";
+        this.languages = [];
+        this.yearMin = 1935;
+        this.yearMax = 2020;
+        this.ageRange = [];
+        this.genres = [];
+        this.ratingMin = 0;
+        this.ratingMax = 100;
         this.data = [];
 
         this.charts = {
@@ -21,6 +28,34 @@ class Main {
     setChartType(chartType) {
         this.chartType = chartType;
         this.render();
+    }
+
+    setLanguages(languages) {
+        this.languages = languages;
+    }
+
+    setYearMin(yearMin) {
+        this.yearMin = yearMin;
+    }
+
+    setYearMax(yearMax) {
+        this.yearMax = yearMax;
+    }
+
+    setAgeRange(ageRange) {
+        this.ageRange = ageRange;
+    }
+
+    setGenres(genres) {
+        this.genres = genres;
+    }
+
+    setRatingMin(ratingMin) {
+        this.ratingMin = ratingMin;
+    }
+
+    setRatingMax(ratingMax) {
+        this.ratingMax = ratingMax;
     }
 
     fetchData() {
@@ -44,4 +79,32 @@ function setDataType(dataType) {
 
 function setChartType(chartType) {
     main.setChartType(chartType);
+}
+
+function setLanguages(languages) {
+    main.setLanguages(languages)
+}
+
+function setYearMin(yearMin) {
+    main.setYearMin(yearMin);
+}
+
+function setYearMax(yearMax) {
+    main.setYearMax(yearMax);
+}
+
+function setAgeRange(ageRange) {
+    main.setAgeRange(ageRange);
+}
+
+function setGenres(genres) {
+    main.setGenres(genres);
+}
+
+function setRatingMin(ratingMin) {
+    main.setRatingMin(ratingMin);
+}
+
+function setRatingMax(ratingMax) {
+    main.setRatingMax(ratingMax);
 }
