b2e4667290c9a6e1953e03e7b813104a927ef566
Ryan Goodman <ryan.goodman@aggiemail.usu.edu>
2020-11-21 13:22:01 -0700

    sunbusrst init

--- js/Sunburst.js
+++ js/Sunburst.js
@@ -1,11 +1,11 @@
 class Sunburst {
     constructor() {
-      let burst = {
+      this.burst = [
           {name:"Netflix", children:[] },
           {name:"Amazon Prime", children:[]},
           {name:"Hulu", children:[]},
           {name:"Disney", children:[]}
-      };
+      ];
     }
 
     render(data, count) {
